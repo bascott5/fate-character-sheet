fa3f84c7434f3b5249ce9fa18510673849337883
Bennett <bascott1002@gmail.com>
2023-05-10 21:50:12 -0700

    update notes feature

--- src/components/context-provider.tsx
+++ src/components/context-provider.tsx
@@ -5,8 +5,8 @@ interface Props {
 }
 
 export type Action = 
-| { type: "toggle", payload: any } 
-| { type: "Save", payload: any };
+| { type: "Save", payload: any } 
+| { type: "Load", payload: any };
 
 export let initState: Object = {
     isAspects: false,
@@ -26,10 +26,9 @@ export const Context = createContext<[Object, React.Dispatch<Action>]>([initStat
 const ContextProvider: React.FC<Props> = ({ children }: Props) => {
     const reducer = (state: Object, action: Action) => {
         switch (action.type) {
-            case "toggle": //remove
-                console.log(state);
-                return {...state, payload: !action.payload}
             case "Save":
+                return {...state}
+            case "Load":
                 return {...state}
             default:
                 return {...state};
@@ -38,11 +37,13 @@ const ContextProvider: React.FC<Props> = ({ children }: Props) => {
 
     const [state, dispatch] = useReducer(reducer, initState);
     return (
-        //button for Save
-        //button for Load
-        <Context.Provider value={[state, dispatch]}>
-            { children }
-        </Context.Provider>
+        <div>
+            <Context.Provider value={[state, dispatch]}>
+                <button onClick={() => dispatch({ type: "Save", payload: 0})}>Save</button>
+                <button onClick={() => dispatch({ type: "Load", payload: 0})}>Load</button>
+                { children }
+            </Context.Provider>
+        </div>
     );
 }
 
--- src/components/notes.tsx
+++ src/components/notes.tsx
@@ -1,8 +1,28 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { Context } from "./context-provider";
 
 const Notes: React.FC = () => {
-    const [notes, setNotes] = useState([]);
-    [...notes, "test"]
+    const [edit, isEdit] = useState<boolean>(false);
+    const [notes, setNotes] = useState<string[]>([]);
+
+    return (
+        <div>
+            <h1>NOTES</h1>
+            <div>
+                <button onClick={() => setNotes([...notes, ""])}>+</button>
+                <button onClick={() => isEdit(edit => edit = !edit)}>Edit</button>
+                {notes.map(key => (
+                    edit ?
+                        <input type="text" value={key} onChange={(e) => setNotes(notes => {
+                            notes[notes.indexOf(key)] = e.target.value;
+                            return [...notes];
+                        })} />
+                    :
+                        <p>{ key }</p>                
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Notes;
