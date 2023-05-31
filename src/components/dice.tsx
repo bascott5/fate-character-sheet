f34b0f5d66648ee59cae20f84a86a71443cb1152
Bennett <bascott1002@gmail.com>
2023-05-31 19:11:03 -0400

    add fate dice function

---
+++ src/components/dice.tsx
@@ -0,0 +1,30 @@
+export const dice = () => {
+    let container: number[] = []
+
+    for (let i = 0; i < 4; i++) {
+        let rng = Math.floor(Math.random() * 6) + 1;
+        switch (rng) {
+            case 1:
+                container.push(-1);
+
+            case 2:
+                container.push(-1);
+
+            case 3:
+                container.push(0);
+
+            case 4:
+                container.push(0);
+
+            case 5:
+                container.push(1);
+
+            case 6:
+                container.push(1);
+
+            default:
+                return [...container];
+        }
+    }
+    return container.reduce((sum, num) => sum + num);
+}
